f920ccdbe5827a105dcfa0cd63b28443fb9d526d
Frandy G Slueue <frandy4ever@gmail.com>
2023-12-12 12:39:36 -0600

    update code error

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,16 @@
 #!/usr/bin/node
 $(function () {
   const amens = {};
-  // Uncheck all checkboxes when the page loads
   $('input:checkbox:checked').prop('checked', false);
 
-  $('input:checkbox').change(function () {
+  $('li input:checkbox').change(function () {
     if ($(this).is(':checked')) {
-      // Create a new object to avoid mutation
       const newAmens = { ...amens };
       newAmens[$(this).data('id')] = $(this).data('name');
-      // Update reference to the object
       Object.assign(amens, newAmens);
     } else {
-       // Create a new object to avoid mutation
       const newAmens = { ...amens };
       delete newAmens[$(this).data('id')];
-      // Update reference to the object
       Object.assign(amens, newAmens);
     }
     $('.amenities h4').text(Object.values(amens).join(', '));
